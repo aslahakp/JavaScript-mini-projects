d44a1e79b50ccedfadaa41ad5debf1ec843c63e9
aslahakp <aslahakp786@gmail.com>
2025-01-17 21:44:54 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+let display = document.getElementById("display");
+
+function addToDisplay(input){
+    display.value += input;
+}
+
+function calculate(){
+    try {
+
+        display.value = eval(display.value);
+        
+    } catch (error) {
+
+        display.value = "Error"
+        
+    }
+}
+
+function clearDisplay(){
+    display.value = "";
+}
